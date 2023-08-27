1f63adfafb17a0f36fc121d4f33c76750335e3e0
Matthieu GUERRA <codaxio@protonmail.com>
2023-08-27 03:31:15 +0200

    add example call to backend api

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+const {data} = await useFetch('http://backend:3000/hello')
 </script>
 
 <template>
-<div class="flex items-center h-full justify-center">
-    <div>Content</div>
+<div class="flex items-center h-full justify-center flex-col">
+    <div>Contessssnt</div>
+    <div>Message from backend: {{ data }}</div>
 </div>
 </template>
