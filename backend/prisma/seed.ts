f1359fe4cdf171331c22e87b9ee8da65f82eac9c
Matthieu GUERRA <codaxio@protonmail.com>
2023-09-04 22:50:28 +0200

    add prisma schema and prismaService in backend

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+//import argon2 from 'argon2';
+
+const prisma = new PrismaClient();
+async function main() {
+  //  await prisma.user.upsert({
+  //    where: {
+  //      email: 'admin@42.fr',
+  //    },
+  //    create: {
+  //      name: 'Admin',
+  //      email: 'admin@42.fr',
+  //      password: argon2.hashSync('admin'),
+  //    },
+  //  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
