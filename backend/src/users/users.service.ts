ee47569d7ddc82c1b46638ba24d32fbc00bd79f0
Matthieu GUERRA <codaxio@protonmail.com>
2023-09-05 03:08:47 +0200

    add users module/controller/service

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { DBService } from 'src/db/db.service';
+import { User, Prisma } from '@prisma/client';
+
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class UsersService {
+  constructor(private db: DBService) {}
+  async create(data: CreateUserDto) {
+    const hash = bcrypt.hashSync(data.password, 10);
+    data.password = hash;
+    return this.db.user.create({
+      data,
+    });
+  }
+
+  findAll() {
+    return this.db.user.findMany({
+      //skip,
+      //take,
+      //cursor,
+      //where,
+      //orderBy,
+    });
+  }
+
+  findOne(id: number) {
+    return this.db.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+  findByEmail(email: string) {
+    return this.db.user.findFirst({
+      where: {
+        email,
+      },
+    });
+  }
+
+  update(id: number, data: UpdateUserDto) {
+    return this.db.user.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  remove(id: number) {
+    return this.db.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  formatUser(user: User) {
+    return {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      status: user.status,
+    };
+  }
+}
