0be8baab75a9c674fcccfa97a42028a40c93d59b
Matthieu GUERRA <codaxio@protonmail.com>
2023-09-06 01:53:58 +0200

    format: test config

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,11 @@
 import {
-  Controller,
-  Post,
-  Request,
-  Get,
-  Body,
-  Res,
-  UseGuards,
+    Controller,
+    Post,
+    Request,
+    Get,
+    Body,
+    Res,
+    UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signup.dto';
@@ -15,34 +15,34 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) {}
 
-  @Post('signup')
-  async signup(@Body() signupDto: SignUpDto) {
-    return await this.authService.signup(signupDto);
-  }
+    @Post('signup')
+    async signup(@Body() signupDto: SignUpDto) {
+        return await this.authService.signup(signupDto);
+    }
 
-  @UseGuards(LocalAuthGuard) // Will use email/pass to retrieve user (look at LocalStrategy) or throw if not valid/found
-  @Post('login')
-  async login(@Request() req, @Res({ passthrough: true }) res: Response) {
-    const data = await this.authService.login(req.user); // req.user is the user returned from LocalStrategy.validate()
-    res.cookie('access_token', data.access_token, {
-      httpOnly: true,
-      sameSite: 'strict',
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    });
-    res.send(data);
-  }
+    @UseGuards(LocalAuthGuard) // Will use email/pass to retrieve user (look at LocalStrategy) or throw if not valid/found
+    @Post('login')
+    async login(@Request() req, @Res({ passthrough: true }) res: Response) {
+        const data = await this.authService.login(req.user); // req.user is the user returned from LocalStrategy.validate()
+        res.cookie('access_token', data.access_token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+        });
+        res.send(data);
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('session')
-  async session(@Request() req) {
-    return req.user;
-  }
+    @UseGuards(JwtAuthGuard)
+    @Get('session')
+    async session(@Request() req) {
+        return req.user;
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Post('logout')
-  async logout(@Res({ passthrough: true }) res: Response) {
-    res.clearCookie('access_token');
-  }
+    @UseGuards(JwtAuthGuard)
+    @Post('logout')
+    async logout(@Res({ passthrough: true }) res: Response) {
+        res.clearCookie('access_token');
+    }
 }
