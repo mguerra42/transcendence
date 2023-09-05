b866d3ab78ef9b30ef8c162caf81f6fd272bfe2d
Matthieu GUERRA <codaxio@protonmail.com>
2023-09-06 01:50:44 +0200

    format: set tab to 4

--- frontend/composables/socket.ts
+++ frontend/composables/socket.ts
@@ -2,29 +2,29 @@ import { defineStore } from 'pinia'
 import { io } from 'socket.io-client'
 
 export const useSocket = defineStore('socket', () => {
-  const client = useClient()
-  const socket = ref()
-  const connect = async () => {
-    socket.value = io('http://localhost:3001', {
-      withCredentials: true,
-    })
-  }
+    const client = useClient()
+    const socket = ref()
+    const connect = async () => {
+        socket.value = io('http://localhost:3001', {
+            withCredentials: true,
+        })
+    }
 
-  const emit = (event: string, data: any) => {
-    if (!socket.value)
-      return console.error('Socket not connected')
-    socket.value.emit(event, data)
-  }
+    const emit = (event: string, data: any) => {
+        if (!socket.value)
+            return console.error('Socket not connected')
+        socket.value.emit(event, data)
+    }
 
-  const on = (event: string, callback: (data: any) => void) => {
-    if (!socket.value)
-      return console.error('Socket not connected')
-    socket.value.on(event, callback)
-  }
+    const on = (event: string, callback: (data: any) => void) => {
+        if (!socket.value)
+            return console.error('Socket not connected')
+        socket.value.on(event, callback)
+    }
 
-  return {
-    connect,
-    emit,
-    on,
-  }
+    return {
+        connect,
+        emit,
+        on,
+    }
 })
