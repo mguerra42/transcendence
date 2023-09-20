b26c9728ad6ad9e118aeeecaf44efb1012e1d115
llepiney <llepiney@student.42.fr>
2023-09-20 16:58:18 +0200

    addFriend from searchbar OK but needs inverse

--- backend/src/friend/friend.controller.ts
+++ backend/src/friend/friend.controller.ts
@@ -1,18 +1,20 @@
-import { Controller, Post, Body, Request } from '@nestjs/common';
+import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
 import { FriendService } from './friend.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('friend')
 export class FriendController {
   constructor(private readonly friendService: FriendService) {}
 
   @Post('add')
-  async addFriend(@Request() req, @Body() friendUsername: string) {
+  @UseGuards(JwtAuthGuard)
+  async addFriend(@Request() req, @Body() friendUsername) {
     const currentUserId = req.user.id; // Récupérez l'ID de l'utilisateur à partir de req.user.id
     console.log(`Current User ID: ${currentUserId}`);
-    console.log('Friendadd : ' + friendUsername);
+    console.log('Friendadd : ', friendUsername);
 
     // Utilisez le service FriendsService pour ajouter l'ami dans la base de données
-    await this.friendService.addFriend(currentUserId, friendUsername);
+    await this.friendService.addFriend(currentUserId, friendUsername.newFriendName);
     return { message: 'Ami ajouté avec succès'};
   }
 }
--- backend/src/friend/friend.service.ts
+++ backend/src/friend/friend.service.ts
@@ -1,26 +1,23 @@
 // friend.service.ts
 
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import { UsersService } from '../users/users.service';
+import { DBService } from 'src/db/db.service';
 
 @Injectable()
 export class FriendService {
   constructor(
-    private prisma: PrismaClient,
+    private prisma: DBService,
     private usersService: UsersService,
   ) {}
 
   async addFriend(currentUserId: number, newFriendUsername: string): Promise<void> {
-    // Recherchez l'ami par nom d'utilisateur
     const friend = await this.usersService.findByEmailOrUsername('', newFriendUsername);
 
     if (!friend) {
-      // L'ami n'a pas été trouvé dans la base de données, gérer l'erreur ici
       throw new Error(`L'ami avec le nom d'utilisateur ${newFriendUsername} n'a pas été trouvé.`);
     }
 
-    // Utilisez l'ID de l'ami pour créer la relation d'amitié
     await this.prisma.friend.create({
       data: {
         userOneId: currentUserId,
@@ -29,6 +26,5 @@ export class FriendService {
     });
   }
 
-  // Ajoutez d'autres méthodes pour gérer les amis ici en utilisant le modèle Prisma
 }
 
