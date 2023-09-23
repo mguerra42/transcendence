2d9b08a24941350fce76ba8412e5d330ca64896d
Djibril Sy <dsy@MacBook-Pro-de-Djibril.local>
2023-09-24 01:17:35 +0200

    Frontend layout chat done

--- frontend/components/Auth/ChatWindow.vue
+++ frontend/components/Auth/ChatWindow.vue
@@ -1,35 +1,70 @@
 <template>
-  <div class="fixed bottom-3 left-3 w-1/2 text-black sm:w-1/3 md:w-1/4 lg:w-1/5 bg-white border-5 font-semi-bold cursor-pointer rounded-lg shadow-lg">
+  
+  <div v-if="!chatVisible" class="fixed bottom-3 left-3 w-30 text-black bg-white border-5 font-semi-bold rounded-lg shadow-lg flex justify-center">
+    <button @click="toggleChat" class="px-2 py-1 bg-white-500 text-blue-600 hover:bg-blue-100 rounded-lg">
+      Chats
+    </button>
+  </div>
+
+  <div v-if="chatVisible" class="fixed bottom-3 left-3 w-1/3 text-black bg-white border-5 font-semi-bold rounded-lg shadow-lg">
+    
     <div class="flex justify-center">
       <button @click="toggleChat" class="px-2 py-1 bg-white-500 text-blue-600 hover:bg-blue-100 rounded-lg">
-        {{ chatVisible ? 'Fermer' : 'Chats' }}
+        Fermer
       </button>
     </div>
-    <div v-if="chatVisible" class="max-h-[50vh] overflow-y-auto p-4">
-      <!-- Chat messages will go here -->
-      <div class="flex flex-col"> <!-- Reverse order to make new messages appear at the bottom -->
-        <div v-for="message in messages" :key="message.id" class="mb-2">
-          <div :class="{ 'ml-auto justify-end': message.sender === 'You', 'mr-auto justify-start': message.sender === 'Server' }">
-            <p class="bg-gray-200 text-sm text-black rounded-lg inline-block p-2">
-              {{ message.text }}
-            </p>
+    
+    <div class="max-h-[50vh] overflow-hidden flex">
+      <!-- User List -->
+      <div class="w-1/3 overflow-y-auto scrollbar-w-2 h-[50vh]">
+        <h2 class="text-m font-semi-bold px-2 py-2 text-center">Connected Users</h2>
+        <div v-for="user in onlineUsersArray" class="mb-2">
+          <div class="bg-white cursor-pointer hover:bg-gray-100 flex justify-center">
+            <button @click="chatWithUser(user)" class="mb-1 mt-2 text-black cursor-pointer">
+              {{ user.username }}
+            </button>
           </div>
         </div>
       </div>
-    </div>
 
-    <!-- Input field for sending messages -->
-    <div v-if="chatVisible" class="p-2">
-      <input
-        v-model="newMessage"
-        @keyup.enter="sendMessage"
-        placeholder="Type your message..."
-        class="w-full p-2 text-sm text-black rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500"
-      />
+      <!-- Chat Window -->
+      <div class="flex flex-col w-2/3 p-4">
+        <div id="chatMessages" ref="chatMessages" class="overflow-y-auto scrollbar-w-2 h-[4/5]">
+          <div class="flex flex-col">
+            <div v-for="message in messages" :key="message.id" class="mb-2">
+              <div :class="{ '': message.sender === 'You', 'mr-auto': message.sender === 'Server' }">
+                <p class="bg-gray-200 text-sm text-black rounded-lg inline-block p-2">
+                  {{ message.text }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 h-[1/5] mt-auto">
+          <input
+            v-model="newMessage"
+            @keyup.enter="sendMessage"
+            placeholder="Type your message..."
+            class="w-full p-2 text-sm text-black rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
+<!-- custom css to make the scrollbar smaller -->
+<style>
+  .scrollbar-w-2::-webkit-scrollbar {
+    width: 0.15rem;
+  }
+  .scrollbar-w-2::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+  .scrollbar-w-2::-webkit-scrollbar-thumb {
+    background: #98acc7;
+  }
+</style>
 
 <script setup lang="ts">
 
@@ -42,6 +77,42 @@ const messages: Ref<{
    text: string }[]> = ref([]); // Provide an initial type
 const newMessage = ref('');
 const chatVisible = ref(true);
+const chatMessages = ref();
+
+const scrollToBottom = () => {
+  if (chatMessages.value === undefined)
+    return ;
+  chatMessages.value.scrollTop = chatMessages.value.scrollHeight;
+};
+
+const clearChat = () => {
+  messages.value = [];
+  scrollToBottom();
+};
+
+const onlineUsersArray = await client.chat.getOnlineUsers();
+const numberOfUsers = onlineUsersArray.length;
+
+const chatWithUser = async (userToMessage : any) => {
+
+    const onlineUsers = await client.chat.getOnlineUsers();
+    
+    for (const user of onlineUsers)
+    {
+        if (user.username === userToMessage.username)
+        {
+            const payload = {
+                sender : auth.session.username,
+                receiver: user.socketId,
+                receiverUsername: user.username,
+                text: 'Test : message received from ' + auth.session.username
+            }
+            socket.emit('chatBox', payload);
+            return ;
+        }
+    }
+};
+
 
 const sendMessage = () => {
   if (newMessage.value.trim() === '') 
@@ -58,8 +129,12 @@ const sendMessage = () => {
     sender: 'You',
     text: newMessage.value,
   });
-  // Clear the input field
+
   newMessage.value = '';
+  //puts the action at the end of the stack list to help with inconsistent DOM updates
+  setTimeout(() => {
+    scrollToBottom();
+  }, 0);
 };
 
 const toggleChat = () => {
@@ -69,6 +144,8 @@ const toggleChat = () => {
 onMounted(async () => {
   await auth.refreshSession();
   await socket.connect();
+
+  scrollToBottom(); // Scroll to the bottom when the component is mounted
 
   socket.on('chatBoxResponse', (data: any) => {
     console.log("received a response from server : ")
