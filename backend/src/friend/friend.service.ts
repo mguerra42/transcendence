c65c1e90634badd70104a770767b784b0c22b5aa
llepiney <llepiney@student.42.fr>
2023-09-26 01:16:57 +0200

    functionnal friendship friend1-inversefriend2

--- backend/src/friend/friend.service.ts
+++ backend/src/friend/friend.service.ts
@@ -20,66 +20,62 @@ export class FriendService {
       throw new Error(`L'ami avec le nom d'utilisateur ${newFriendUsername} n'a pas été trouvé.`);
     }
 
-    // Obtenez les ID des deux utilisateurs impliqués
-    const userOneId = currentUserId;
-    const userTwoId = friend.id;
-
     // Créez une nouvelle entrée dans le modèle Friend
     await this.prisma.friend.create({
       data: {
-        userOneId,
-        userTwoId,
+        userOneId : currentUserId,
+        userTwoId : friend.id,
       },
     });
 
-    // Mettez à jour la liste d'amis des deux utilisateurs
-    await this.updateUserFriends(userOneId, userTwoId);
-    await this.updateUserFriends(userTwoId, userOneId);
+    // // Mettez à jour la liste d'amis des deux utilisateurs
+    // await this.updateUserFriends(userOneId, userTwoId);
+    // await this.updateUserFriends(userTwoId, userOneId);
   }
 
-  //PARTIAL BUGFIX 
-  private async updateUserFriends(userId: number, friendId: number): Promise<void> {
-    // Obtenez l'utilisateur actuel
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-      include: {
-        friends: true, // Assurez-vous d'inclure la relation "friends"
-      },
-    });
+  // //PARTIAL BUGFIX 
+  // private async updateUserFriends(userId: number, friendId: number): Promise<void> {
+  //   // Obtenez l'utilisateur actuel
+  //   const user = await this.prisma.user.findUnique({
+  //     where: {
+  //       id: userId,
+  //     },
+  //     include: {
+  //       friends: true, // Assurez-vous d'inclure la relation "friends"
+  //     },
+  //   });
 
-    // Recup l'objet de l'utilisateur dont on va update la friendlist
-    const friend = await this.usersService.findByEmail(user.email);
+  //   // Recup l'objet de l'utilisateur dont on va update la friendlist
+  //   const friend = await this.usersService.findByEmail(user.email);
 
-    // Vérifiez si l'ami n'est pas déjà dans la liste
-    const isFriendAlreadyAdded = user.friends.some((friend:any) => friend.id === friendId);
+  //   // Vérifiez si l'ami n'est pas déjà dans la liste
+  //   const isFriendAlreadyAdded = user.friends.some((friend:any) => friend.id === friendId);
 
-    //recuperer sa friend list
-    const friendlist = user.friends;
+  //   //recuperer sa friend list
+  //   const friendlist = user.friends;
     
-    //update sa friendlist
-    //FIX PARTIEL : je maitrise pas la relation friends donc je laisse ca pour plus tard
-    friendlist.push();
+  //   //update sa friendlist
+  //   //FIX PARTIEL : je maitrise pas la relation friends donc je laisse ca pour plus tard
+  //   friendlist.push();
 
-    //On cree une interface pour update l'objet user sans causer une erreur de type
-    //FIX PARTIEL : A matcher avec la definition de l'objet userToUpdateDto dans le model
-    interface userToUpdateObject {
-      email?: string;
-      password?: string;
-      username?: string;
-      avatarPath?: string;
-      friends?: string;
-      status?: string;
-    }
+  //   //On cree une interface pour update l'objet user sans causer une erreur de type
+  //   //FIX PARTIEL : A matcher avec la definition de l'objet userToUpdateDto dans le model
+  //   interface userToUpdateObject {
+  //     email?: string;
+  //     password?: string;
+  //     username?: string;
+  //     avatarPath?: string;
+  //     friends?: string;
+  //     status?: string;
+  //   }
 
-      //On crée l'objet userToUpdate qui va contenir les infos a update
-      const userToUpdate: userToUpdateObject = {};
-      //FIX PARTIEL : a decommenter apres avoir fix la relation friends
-      // userToUpdate.friends = friendlist;
-      //On fait l'update
-      await this.usersService.update(user.id, userToUpdate);
-  }
+  //     //On crée l'objet userToUpdate qui va contenir les infos a update
+  //     const userToUpdate: userToUpdateObject = {};
+  //     //FIX PARTIEL : a decommenter apres avoir fix la relation friends
+  //     // userToUpdate.friends = friendlist;
+  //     //On fait l'update
+  //     await this.usersService.update(user.id, userToUpdate);
+  // }
 }
 
 
