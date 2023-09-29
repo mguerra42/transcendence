94b2525c9308109fc06b25fe0f1f6042b120786e
anais <anais>
2023-09-29 13:56:40 +0200

    improve UserProfile

--- frontend/components/Chat/UserProfile.vue
+++ frontend/components/Chat/UserProfile.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 const client = useClient();
-//const auth = useAuth();
+
 </script>
 
 <template>
     <!-- div principale -->
-    <div class="w-100 bg-zinc-700 p-6 rounded-lg relative flex-col">
+    <div class="w-100 bg-zinc-700 px-2 py-6 rounded-lg relative flex-col">
         <!-- div user/bouton -->
         <div class="flex justify-between">
             <!-- div bouton pour fermer -->
@@ -15,49 +15,41 @@ const client = useClient();
             </div>
             <!-- div avatar user -->
             <div class="flex flex-col justify-center p-2 ">
-                <img :src="client.chat.chatState.receiver.avatarPath" class="w-12 h-12 rounded-full" />
+                <img :src="client.chat.chatState.receiver.avatarPath" class="w-14 h-14 rounded-full" />
             </div>
             <!-- div bouton -->
             <div class=" p-2 ">
-                <button class="bg-zinc-600 hover:bg-zinc-500 rounded-lg text-zinc-200 text-sm px-2 py-2 mt-2">
-                    <div class="flex flex-col">
+                <button class="bg-zinc-600 hover:bg-zinc-500 rounded-lg text-zinc-200 text-sm px-4 py-2 mt-2">
+                    <div class="flex">
                         <div class="i-mdi:account-multiple-plus"></div>
-                    </div>
-                    <div class="flex flex-col">
-                        <p class=" text-md text-zinc-200" >Add friend</p>
+                        <p class=" text-md text-zinc-200 pl-3" >Add friend</p>
                     </div>
                 </button>
             </div>
         </div>
 
         <!-- div rectangle noir -->
-        <div class="top-4 bg-zinc-800 p-6 rounded-lg relative flex-col">
+        <div class="top-4 bg-zinc-800 px-3 py-4 rounded-lg relative flex-col">
             <!-- div pseudo -->
-            <div class="flex flex-col justify-center">
-                <p class=" text-md text-zinc-200" >{{ client.chat.chatState.receiver.username }}</p>
-                <p class=" text-xs text-zinc-400" >ELO : {{ client.chat.chatState.receiver.ladderPoint}}</p>
+            <div class="flex flex-col justify-center ">
+                <p class=" text-lg text-zinc-200" >{{ client.chat.chatState.receiver.username }}</p>
+                <!-- <p class=" text-xs text-zinc-400" >ELO : {{ client.chat.chatState.receiver.ladderPoint}}</p> -->
             </div>
             <!-- div boutons -->
-            <div class="flex justify-between">
-                <button class=" hover:bg-zinc-500 rounded-lg text-zinc-200 text-sm px-2 py-2 mt-2">
-                    <div class="flex flex-col">
-                        <p class=" text-md text-zinc-200" >Bouton 1</p>
-                    </div>
+            <div class="flex border-b border-zinc-600">
+                <button class="border-b border-zinc-800 hover:border-zinc-200 text-zinc-200 mt-2 mr-5 py-3">
+                        <p class=" text-sm text-zinc-200" >Informations</p>
                 </button>
-                <button class="hover:bg-zinc-500 rounded-lg text-zinc-200 text-sm px-2 py-2 mt-2">
-                    <div class="flex flex-col">
-                        <p class=" text-md text-zinc-200" >Bouton 2</p>
-                    </div>
+                <button class="border-b border-zinc-800 hover:border-zinc-200  text-zinc-200 mt-2 mr-5 py-3">
+                        <p class=" text-sm text-zinc-200" >Common channels</p>
                 </button>
-                <button class=" hover:bg-zinc-500 rounded-lg text-zinc-200 text-sm px-2 py-2 mt-2">
-                    <div class="flex flex-col">
-                        <p class=" text-md text-zinc-200" >Bouton 3</p>
-                    </div>
+                <button class=" border-b border-zinc-800 hover:border-zinc-200 text-zinc-200 mt-2 mr-5 py-3">
+                        <p class=" text-sm text-zinc-200" >Game history</p>
                 </button>
             </div>
             <!-- div info selon bouton -->
             <div class="flex flex-col justify-center">
-                <p class=" text-md text-zinc-200 py-6" >Info selon bouton</p>
+                <p class=" text-sm text-zinc-200 py-6" >Info selon bouton</p>
             </div>
         </div>
     </div>
