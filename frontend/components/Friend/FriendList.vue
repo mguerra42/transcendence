8b39a8333c43ec5502a9fd0e793e55f8423b9007
llepiney <llepiney@student.42.fr>
2023-10-04 00:45:39 +0200

    visual changes+ exit cross+ showfriend only if logged

--- frontend/components/Friend/FriendList.vue
+++ frontend/components/Friend/FriendList.vue
@@ -48,41 +48,41 @@ onMounted(() => {
 </script>
 
 <template>
-  <div v-if="friend.showFriend" class="fixed bg-gray-700 rounded-md left-50 top-30 p-4 h-70">
+  <div v-if="friend.showFriend" class="fixed min-h-90 max-h-90 bg-gray-700 rounded-t-md right-30 bottom-10 p-4 h-70">
     <div class="flex">
 
     <input
     v-model="newFriendName"
     type="text"
     placeholder="Rechercher un ami"
-    class="block px-4 h-10 py-2 mb-4 border border-gray-300 rounded-md"
+    class="block px-4 h-10 py-2 mb-4 w-45 border border-gray-300 rounded-md"
     />
     <button @click="addFriend(newFriendName)" class="px-4 py-1 h-10 ml-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">
     Add
     </button>
     <div @click="friend.showFriend = false"
-            class="absolute top--2 right--2 right p-1 cursor-pointer hover:text-white rounded m-1 text-gray-400 h-10 ">
+            class="absolute  top--2 right--2 right p-1 cursor-pointer hover:text-white rounded m-1 text-gray-400 h-10 ">
             <div class="i-mdi:close"></div>
         </div>
     </div>
-    <div class="bg-gray-700 p-4 rounded-lg">
+    <div class="bg-gray-700  rounded-lg  ">
       <div class="mb-4 flex">
         <div class="flex items-center">
           <span class="text-white font-bold cursor-pointer hover:text-blue-500 transition duration-300" @click="fetchFriendlist('amis')">Amis</span>
         </div>
 
-        <div class="flex items-center ml-4">
+        <div class="flex items-center ml-4 mt-n4">
           <span class="text-white font-bold cursor-pointer hover:text-blue-500 transition duration-300" @click="fetchFriendlist('enAttente')">En attente</span>
         </div>
 
-        <div class="flex items-center ml-4">
+        <div class="flex items-center ml-4 mt-n4">
           <span class="text-white font-bold cursor-pointer hover:text-blue-500 transition duration-300" @click="fetchFriendlist('demandes')">Demandes</span>
         </div>
       </div>
 
 
       <div class="grid grid-cols-1 gap-2">
-      <div class="max-h-64 overflow-y-auto"> <!-- max-h-64 limite la hauteur maximale -->
+      <div class="max-h-64 overflow-y-auto">
         <div v-for="item in currentCategory" class="text-white bg-gray-700 p-2 shadow hover:bg-gray-600 flex justify-between items-center">
             <div>{{ item.username }}</div>
             <div v-if="categoryTab === 'demandes'" class="flex space-x-2">
@@ -98,7 +98,8 @@ onMounted(() => {
       </div>
 
 
+    </div>
   </div>
-  </div>
+
   </template>
   
