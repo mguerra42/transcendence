f271709d306032c80d74f40bda6b473517df7fee
Djibril Sy <dsy@MacBook-Pro-de-Djibril.local>
2023-10-18 01:18:57 +0200

    Added responsive login and home page, add rresponsive pong next. Added socket.connect to new home page

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
-  <main class="h-100vh overflow-y-auto scrollbar-w-2">
+  <main class="h-100vh">
     
     <!-- Logged In View -->
-    <div v-if="auth.logged === true">
+    <div v-if="auth.logged === true" class="overflow-y-auto scrollbar-w-2">
       <Header :gameProps="gameProps" :stateProps="stateProps" />
         <div class="h-[calc(100vh-80px)] flex" >
           
           <!-- Left Sidebar -->
-            <div class="w-1/6 rounded bg-zinc-800 max-h-[100vh] overflow-y-auto scrollbar-w-2">
+            <div class="w-0/6 lg:w-300px sm:w-0/6 md:w-0/6 rounded bg-zinc-800 max-h-[100vh] overflow-y-auto scrollbar-w-2">
               <!-- Leaderboard -->
-              <div class="overflow-y-auto scrollbar-w-2 h-[70vh] flex justify-center bg-zinc-900 rounded-lg m-4">
-                <p class="text-4xl text-center font-bold m-2">
+              <div class="overflow-y-auto scrollbar-w-2 h-[70vh] bg-zinc-900 rounded-lg m-4">
+                <p class="text-4xl flex text-center font-bold m-2">
                   Leaderboard
                 </p>
               </div>
@@ -18,13 +18,13 @@
           <!-- Left Sidebar -->
           
           <!-- Nuxt Page -->
-            <div class="flex-1 w-4/6 bg-red">
+            <div class="flex-1 w-4/6 bg-red overflow-y-auto scrollbar-w-2">
               <slot />
             </div>
           <!-- Nuxt Page -->
           
           <!-- Right Sidebar -->
-          <div class="w-1/6 rounded bg-zinc-800 max-h-[100vh] overflow-y-auto scrollbar-w-2">
+          <div class="w-0/6 sm:w-0/6 lg:w-300px md:w-300px rounded bg-zinc-800 max-h-[100vh] overflow-y-auto scrollbar-w-2">
             <div class="overflow-y-auto scrollbar-w-2 h-[100vh] bg-zinc-900 rounded-lg m-4">
               <p class="text-4xl text-center m-2">
                   {{ auth.session.username }}
@@ -35,18 +35,18 @@
           
           <AuthModal/>
         </div>
-        <Footer/>
+      <Footer/>
     </div>
     
     <!-- Logged Out View -->
       <div v-else class="h-full rounded-lg flex">
-        <div class="min-h-screen w-3/4 bg-zinc-300 flex flex-col items-center justify-center">
+        <div class="min-h-screen w-0/5 lg:w-3/5 md:w-1/2 sm:w-0/5 xs:w-0/5 bg-zinc-300 flex flex-col items-center justify-center">
           <div>
             <p class="text-7xl text-center font-bold text-zinc-600 m-4">transcendence</p>
           </div>
         </div>
-        <div class="min-h-screen w-1/4 bg-zinc-800 flex flex-col items-center justify-center">
-          <div class="flex justify-center">
+        <div class="min-h-screen w-5/5 lg:w-2/5 md:w-1/2 sm:w-5/5 xs:w-5/5 bg-zinc-800 flex flex-col items-center justify-center">
+          <div class="">
             <AuthLoginForm v-if="auth.mode === 'login'" />
             <AuthSignUpForm v-else />
           </div>
@@ -63,16 +63,12 @@
 </style>
 
 <script setup lang="ts">
-import LoginForm from '~/components/Auth/LoginForm.vue';
-
   const auth = useAuth()
-
   const { stateProps, gameProps } = defineProps<{
       stateProps: any,
       gameProps: any
   }>();
 
-  onMounted(() => {
-    // test.printTest()
+  onMounted(async() => {
   })
 </script>
