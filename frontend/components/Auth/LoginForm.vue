f715a8a85302de90a2bb1b1f61aa57d1c0ff8280
llepiney <llepiney@student.42.fr>
2023-11-10 14:53:21 +0100

    progressed menu

--- frontend/components/Auth/LoginForm.vue
+++ frontend/components/Auth/LoginForm.vue
@@ -20,6 +20,8 @@ const login42 = async () => {
     client.auth.authMethod = 'intra'
 }
 
+import '/frontend/public/styles/home.css';
+
 </script>
 <template>
     <div class="p-6 relative">
@@ -27,30 +29,36 @@ const login42 = async () => {
             class="text-2xl p-1 hover:bg-blue-500 cursor-pointer hover:text-white rounded m-1 text-black absolute right-0 top-0">
             <div class="i-mdi:close"></div>
         </div> -->
-        <h1 class="text-black text-2xl">
-            Welcome Back ! 😊 
+        <h1 class="home-font text-2xl">
+            Welcome back ! 😊 
         </h1>
-        <h1 class="text-gray text-2xl mb-6">Log In to Your Account</h1>
+        <h1 class="home-font text-2xl mb-6">Log in to your account</h1>
         <form @submit.prevent="client.auth.authenticateUser({
                 email: email,
                 password: password
             })" class="flex flex-col space-y-4" autocomplete="off">
-            <input type="email" v-model="email" name="email" placeholder="Email" class="rounded-lg px-3 py-2 text-black b-1 " />
-            <input type="password" v-model="password" name="password" placeholder="Password"
-                class="rounded-lg px-3 py-2 text-black  b-1" />
+            <div style="display: flex; align-items: baseline;">
+                <span class="home-font">Email:</span>
+                <input type="email" v-model="email" name="email" placeholder="placeholder" class="px-3 py-2 text-black b-1 w-80" style="background-color: transparent; border:none;"/>
+            </div>
+            <div style="display: flex; align-items: baseline;">
+                <span class="home-font">Password:</span>
+                <input type="password" v-model="password" name="password" placeholder="Password placeholder"
+                    class="px-3 py-2 text-black  b-1" style="background-color: transparent; border:transparent;"/>
+            </div>
             <div class="text-red">
                 {{ auth.error }}
             </div>
 
             <!-- Default Login-->
-            <button type="submit" :disabled="!isValid" class="bg-blue-500 text-white rounded-lg  cursor-pointer hover:scale-105 transition px-4 py-2" :class="{
-                'hover:bg-blue-600 transition duration-300': isValid,
-            }">
-                Log In
+            <button class="home-font cursor-pointer hover:scale-105 transition" :class="{
+                'transition duration-300': isValid,
+            }" style="text-align:left">
+                Log in
             </button>   
 
             <!-- Google Login-->
-            <button type="button" @click="loginGoogle" class="bg-white-500 border-2 border-gray-300 text-gray-600 font-semibold rounded-lg cursor-pointer hover:scale-105 transition px-4 py-2 flex justify-center items-center">
+            <button type="button" @click="loginGoogle" class="border-gray-300 text-gray-600 font-semibold rounded-lg cursor-pointer hover:scale-105 transition px-4 py-2 flex">
             <img src="../../public/google_signin_logo.png" alt="Google Logo" class="w-6 h-6 mr-2" /> Sign in with Google
             </button>
 
@@ -59,9 +67,9 @@ const login42 = async () => {
             <img src="../../public/42_Logo.png" alt="42 Logo" class="w-6 h-6 mr-2" />   Sign in with Intra
             </button> -->
             
-            <h1 class="text-gray text-2s flex justify-center">or</h1>
-            <a href="#" class="text-blue-500 hover:text-blue-600 flex justify-center" @click="auth.mode = 'signup'">
-                Sign Up
+            <!-- <h1 class="text-gray text-2s flex justify-center">or</h1> -->
+            <a href="#" class="home-font flex" @click="auth.mode = 'signup'">
+                Sign up
             </a>
         </form>
     </div>
