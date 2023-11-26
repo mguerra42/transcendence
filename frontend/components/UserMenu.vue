1cfc17b2cd5b7be481a96c179f022d37a39eedbf
llepiney <llepiney@student.42.fr>
2023-11-26 19:34:38 +0100

    user settings part desgined

--- frontend/components/UserMenu.vue
+++ frontend/components/UserMenu.vue
@@ -4,14 +4,14 @@ const auth = useAuth()
 
 </script>
 <template>
-    <div  class="absolute top-100% right-0 bg-white text-dark min-w-160px  rounded-lb overflow-hidden z-500" >
+    <div  class="absolute top-100% right-0 term-box text-dark min-w-160px  rounded-lb overflow-hidden z-500" >
         <ul>
             <li class="b-t-1 select-none hover:bg-gray-200 hover:text-gray-800 px-3 py-2 cursor-pointer">
                 <div class="font-bold">{{ auth.session.email }}</div>
                 <div >@{{ auth.session.username }}</div>
             </li>
             <li class="">
-                <nuxt-link to="/settings" class="flex items-center gap-2 b-t-1 select-none hover:bg-gray-200 hover:text-gray-800 px-3 py-2 cursor-pointer">
+                <nuxt-link to="/settings" class="flex items-center gap-2 b-t-1 select-none hover:bg-gray-300 hover:text-gray-800 px-3 py-2 cursor-pointer">
                     <div class="i-mdi:cogs"></div>
                     <div>Settings</div>
                 </nuxt-link>
--- frontend/pages/settings.vue
+++ frontend/pages/settings.vue
@@ -81,12 +81,14 @@ const resetAvatar = () => {
     auth.session.avatar = '/avatars/default.jpg'
 }
 
+import '/frontend/public/styles/home.css';
+
 </script>
 <template>
     <div>
-        <div class="w-120 mx-auto my-5 bg-white p-6 rounded-lg relative text-black">
+        <div class="w-122 mx-auto my-5 term-box p-6 relative text-black">
 
-        <h1 class="text-black text-2xl" mb-4>
+        <h1 class="home-font text-2xl" style="letter-spacing : 2px" mb-4>
             My Profile
         </h1>
         <!-- <h1 class="text-gray text-2xl mb-2">You can update your profile here</h1>  -->
@@ -96,29 +98,34 @@ const resetAvatar = () => {
         })" class="flex flex-col gap-4" autocomplete="off">
             
             <div class="flex flex-col">
-                <div class="font-bold text-base mt-4">Change your profile picture (optional) [
-                    <button class="text-blue-600 text-sm" type="button" @click="resetAvatar">Reset Avatar</button>
+                <div class="home-font text-base mt-4">Change your profile picture (optional) [
+                    <button class="text-white/50 home-button layers hero glitch text-sm" data-text="reset Avatar" type="button" @click="resetAvatar">Reset Avatar</button>
                     ]</div>
                 <div class="flex justify-center">
-                    <img :src="avatarPreview" class="rounded-lg mt-4 h-100px max-w-100px b-2 b-white p-.5" />
+                    <img :src="avatarPreview" class="mt-4 h-100px max-w-100px b-2 b-white p-.5" />
                 </div>
-                <input type="file" accept="image/jpeg,image/png" ref="fileInput"  @change="onFileSelected"  />
+                <div class="flex flex-row pt-2">
+                    <input type="file" accept="image/jpeg,image/png" name="file" id="file" class="inputfile" ref="fileInput"  @change="onFileSelected" />
+                    <label for="file" class="home-font home-button layers hero glitch ml-3" style="letter-spacing : 1px;" data-text="Choose a file">Choose a file</label>
+                    <div class="i-material-symbols:file-copy-outline-sharp ml-2 home-font"></div>
+                </div>
             </div>
+            
             <div class="flex flex-col gap-1.5">
-                <label class="font-bold text-base ">Username</label>
-                <input type="text" @input="onInput" v-model="payload.username" name="username" placeholder="New username" class="rounded-lg px-3  py-2 text-black b-1 " />
+                <label class="font-bold home-font text-base ">Username</label>
+                <input type="newUsername" @input="onInput" v-model="payload.username" name="username" placeholder="New username" class="px-3  py-2 text-black b-1 " />
             </div>
             <div class="flex flex-col gap-1.5">
 
-                <label class="font-bold text-base ">Email</label>
-                <input type="text" v-model="payload.email" name="email" placeholder="New email" class="rounded-lg px-3 py-2 text-black b-1 " />
+                <label class="font-bold home-font text-base ">Email</label>
+                <input type="newEmail" v-model="payload.email" name="email" placeholder="New email" class="px-3 py-2 text-black b-1 " />
             </div>
             <div class="flex flex-col gap-1.5">
 
                 <label class="relative inline-flex items-center cursor-pointer">
                 <input type="checkbox" :onChange="onChangePassword" v-model="payload.changePassword" class="sr-only peer" checked>
                 <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span class="ms-3 font-bold text-base text-gray-900 dark:text-gray-900">Change password</span>
+                <span class="ms-3 font-bold text-base home-font text-white/50 dark:text-white/50">Change password</span>
                 </label>
             </div>
             <div class="flex flex-col gap-1.5" v-if="payload.changePassword">
@@ -130,7 +137,7 @@ const resetAvatar = () => {
                 <label class="relative inline-flex items-center cursor-pointer">
                 <input type="checkbox" :onChange="onMfaToggle" v-model="payload.mfaEnabled" class="sr-only peer" checked>
                 <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span class="ms-3 font-bold text-base text-gray-900 dark:text-gray-900">
+                <span class="ms-3 font-bold home-font text-base text-white/50 dark:text-white/50">
                     {{ payload.mfaEnabled ? 'Disable 2FA' : 'Enable 2FA'  }}
                 </span>
                 </label>
@@ -148,15 +155,15 @@ const resetAvatar = () => {
                 </div>
                 <div class="flex flex-col gap-1.5">
 
-                <label class="font-bold text-base ">Current password</label>
-                <input type="password" v-model="payload.password" name="password" placeholder="password" class="rounded-lg px-3 py-2 text-black b-1 " />
+                <label class="font-bold home-font text-base ">Current password</label>
+                <input type="password" v-model="payload.password" name="password" placeholder="password" class="px-3 py-2 text-black b-1 " />
             </div>
 
 
-            <button type="submit" class=" text-white rounded-lg  cursor-pointer hover:scale-105 transition px-4 py-2" :class="{
-                'hover:bg-blue-600 bg-blue-500 transition duration-300': isValid,
-                'bg-gray-500': isValid === false,
-            }">
+            <button type="submit" class="home-font home-button layers hero glitch cursor-pointer transition px-4 py-2" :class="{
+                'transition duration-300': isValid,
+                'term-box': isValid === false,
+            }" data-text="Update profile">
                 Update profile
             </button>
             <!-- Add login button and other login form fields here -->
@@ -164,3 +171,21 @@ const resetAvatar = () => {
     </div>
     </div>
 </template>
+
+<style scoped>
+.inputfile {
+	width: 0.1px;
+	height: 0.1px;
+	opacity: 0;
+	overflow: hidden;
+	position: absolute;
+	z-index: -1;
+}
+
+.inputfile + label {
+    font-size: 1.25em;
+    font-weight: 700;
+    display: inline-block;
+    text-align : center;
+}
+</style>
